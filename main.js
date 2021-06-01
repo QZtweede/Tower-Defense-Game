d8f36759fe181069109b2d34cb31f0d8c0c61804
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-06-01 14:47:39 +0200

    start health systeem

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 var canvas = document.getElementById("canvas")
 var buttonClicked = document.getElementById("startButton").addEventListener("click", startWave)
+var health = 100
+var enemy
 
-function startWave(){
-    console.log("test")
-    var enemy = document.createElement("div")
-    canvas.appendChild(enemy)
-    enemy.setAttribute("class", "enemy")
+
+
+async function startWave(){
+    for(var i = 0; i < 5; i++){
+        enemy = document.createElement("div")
+        canvas.appendChild(enemy)    
+        enemy.setAttribute("class", "enemy")
+        await sleep(1000)
+    }
+    checkHealth()
 }
+
+async function checkHealth(){
+    if( parseInt(getComputedStyle(enemy).left) >= 40){
+        health--
+    }
+    sleep(100)
+    checkHealth()   
+}
+
+
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }

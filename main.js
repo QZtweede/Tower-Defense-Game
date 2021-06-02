2496f932606380f7c501e9e306001f8f053ede99
Rovers,Zjuul Z <z.rovers@student.fontys.nl>
2021-06-02 11:14:14 +0200

    check health, destroy enemies buiten de range

--- main.js
+++ main.js
@@ -2,27 +2,37 @@ var canvas = document.getElementById("canvas")
 var buttonClicked = document.getElementById("startButton").addEventListener("click", startWave)
 var health = 100
 var enemy
-
-
+var enemyVal = 0
+var firstEnemyVal
+var enemyArray = []
 
 async function startWave(){
-    for(var i = 0; i < 5; i++){
-        enemy = document.createElement("div")
-        canvas.appendChild(enemy)    
-        enemy.setAttribute("class", "enemy")
-        await sleep(1000)
-    }
-    checkHealth()
+    if(enemyArray.length === 0){
+        for(var i = 0; i < 5; i++){
+            enemy = document.createElement("div")
+            canvas.appendChild(enemy)    
+            enemy.setAttribute("class", "enemy")
+            enemy.setAttribute("id", "enemy" + enemyVal)
+            enemyArray.push(enemyVal)
+            enemyVal++     
+            await sleep(1000)
+        }
+        checkHealth()
+    }  
 }
 
 async function checkHealth(){
-    if( parseInt(getComputedStyle(enemy).left) >= 40){
+    if( parseInt(getComputedStyle(enemy).left) >= 300){
         health--
-    }
-    sleep(100)
-    checkHealth()   
+        document.getElementById("health").innerHTML = "♡ 0" + health
+        firstEnemyVal = Math.min.apply(null, enemyArray)
+        var firstEnemy = document.getElementById("enemy" + firstEnemyVal)
+        firstEnemy.remove()
+        enemyArray.shift()
+    } 
+    await sleep(1000)
+    checkHealth()
 }
-
 
 function sleep(time) {
     return new Promise(resolve => setTimeout(resolve, time));
